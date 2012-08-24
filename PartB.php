d4f9dc3fdf19e47c9be1c26900efc6ac48368116
SuchangZhou <neard0325@163.com>
2012-08-24 21:51:42 +1000

    SearchBar added

--- PartB.php
+++ PartB.php
@@ -1,24 +1,33 @@
 <html>
 <head>
-<title>Wines(PartB)</title>
+<title>Wines Search Page</title>
 </head>
 <body>
+	<form action ="PartBresult.php" method = "GET">
+	Wine:<input type = "text" name = "wine_name" /> <br />
+	Winery:<input type = "text" name = "winery_name" /> <br />
+	Min in stock:<input type = "text" name = "min_stock" /> <br />
+	Max in stock:<input type = "text" name = "max_stock" /> <br />
+	Min Cost:<input type = "text" name = "min_cost" /> <br />
+	Max Cost:<input type = "text" name = "max_cost" /> <br />
+  
+  
 <?php
-  require_once('db.php');
+	require_once('db.php');
 
   // (1) Open the database connection
-  $connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
-  mysql_select_db("winestore", $connection);
+  	$connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
+	mysql_select_db("winestore", $connection);
 
-
-
-
+  
 
 
 
 
   // (4) Close the database connection
-  mysql_close($connection);
+  	mysql_close($connection);
 ?>
+	<input type ="submit" value = "Search" /> <br />
+</form>
 </body>
 </html>
