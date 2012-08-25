32348365fadb735f02f663585857e0d752aa061a
SuchangZhou <neard0325@163.com>
2012-08-25 15:31:44 +1000

    Drop down list(region for test) added

--- PartB.php
+++ PartB.php
@@ -1,33 +1,41 @@
 <html>
-<head>
-<title>Wines Search Page</title>
-</head>
-<body>
-	<form action ="PartBresult.php" method = "GET">
-	Wine:<input type = "text" name = "wine_name" /> <br />
-	Winery:<input type = "text" name = "winery_name" /> <br />
-	Min in stock:<input type = "text" name = "min_stock" /> <br />
-	Max in stock:<input type = "text" name = "max_stock" /> <br />
-	Min Cost:<input type = "text" name = "min_cost" /> <br />
-	Max Cost:<input type = "text" name = "max_cost" /> <br />
-  
-  
+  <head>
+    <title>Wines Search Page</title>
+  </head>
+  <body>
+    <form action ="PartBresult.php" method ="GET">
+      A wine name(or part of a wine name):<input type = "text" name = "wine_name" id = "wine_name" /> <br /> <br />
+      A winery name (or part of a winery name):<input type = "text" name = "winery_name" id = "winery_name" /> <br /> <br />
+      A minimum number of wines in stock:<input type = "text" name = "min_stock" id = "min_stock"/> <br /> <br />
+      A minimum number of wines ordered:<input type = "text" name = "min_order" id = "min_order"/> <br /> <br />
+      A minimum dollar cost range:<input type = "text" name = "min_cost" id = "mincost"/> <br /> <br />
+      A maximum dollar cost range:<input type = "text" name = "max_cost" id = "maxcost"/> <br /> <br />
+    
 <?php
-	require_once('db.php');
+      // Open the database connection
+      require_once('db.php');
+      $connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
+      mysql_select_db("winestore", $connection);
+      
+      $query = "SELECT region_name from region";
+      $result = mysql_query($query, $connection);
+      
+      echo "Choose a region:";
+      echo '<select>';
+        while ($row = mysql_fetch_row($result)){
+            for($i = 0; $i < mysql_num_fields($result); $i++){
+              echo "<option value = '$row[$i]'>$row[$i]</option>";
+            }
+        }
+      echo '</select>';
+      echo "<br />";
+      
+      
+      //Close the database connection
+      mysql_close($connection);
 
-  // (1) Open the database connection
-  	$connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
-	mysql_select_db("winestore", $connection);
-
-  
-
-
-
-
-  // (4) Close the database connection
-  	mysql_close($connection);
 ?>
-	<input type ="submit" value = "Search" /> <br />
-</form>
-</body>
+      <input type ="submit" value = "Search" /> <br />
+    </form>
+  </body>
 </html>
