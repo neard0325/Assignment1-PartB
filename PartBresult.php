758f514be2fb63fe40e47c1bf90eba0bf01c2323
SuchangZhou <neard0325@163.com>
2012-08-27 12:43:57 +1000

    Query edit ver1.1

--- PartBresult.php
+++ PartBresult.php
@@ -6,24 +6,30 @@
     
     
 <?php
+
+	  function showerror() {
+			die("Error " . mysql_errno() . " : " . mysql_error());
+	  }
       // Open the database connection
       require_once('db.php');
-	  if(!$connection = mysql_connect(DB_HOST, DB_USER, DB_PW)){
-		echo 'Could not connect to mysql on ' . DB_HOST . "\n";
-		exit;
-	  }
-	  else{
-      $connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
-      mysql_select_db("winestore", $connection);
-	  }
+	  $connection = mysql_connect(DB_HOST, DB_USER, DB_PW);
+	  mysql_select_db('winestore', $connection);
 
 	  //table create
 	  function display_table($connection, $query){
+	  // Run the query on the server
+	  if(!($result = @ mysql_query($query, $connection))){
+			showerror();
+			echo "<br>";
+	  }
+	  // Find out how many rows are available
+	  $rowFound = @ mysql_num_rows($result);
 
-	  $result_list = mysql_query($query, $connection);
-	  $rowFound = mysql_num_rows($result_list);
-
-	  echo "<table width='1250' cellpadding='5' cellspacing='5' border='2'>";
+	  if($rowFound > 0){
+		echo "Search Result as below";
+		echo "<br>";
+		echo $rowFound . " datas found in database";
+		echo "<table width='1250' cellpadding='5' cellspacing='5' border='2'>";
 		echo "<tr><td width = '150'><b>Name</b></td>
 				  <td width = '100'><b>Variety</b></td>
 				  <td width = '100'><b>Year</b></td>
@@ -34,7 +40,7 @@
 				  <td width = '100'><b>Total stock</b></td>
 				  <td width = '150'><b>Total revenue</b></td></tr>";
 
-			while($row = @ mysql_fetch_array($result_list)){
+			while($row = @ mysql_fetch_array($result)){
 				echo "<tr><td width = '150'>{$row["wine_name"]}</td>
 						  <td width = '100'>{$row["variety"]}</td>
 						  <td width = '100'>{$row["year"]}</td>
@@ -48,67 +54,81 @@
 			}
 		echo "</table>";
 		}
+		else{
+			echo " No matched record";
+		}
+	   }
 
-		$query = "Select wine_name, variety, year, winery_name, region_name, cost, sum(qty), on_hand, sum(price)
-          From wine, grape_variety, winery, region, inventory, items, wine_variety
+	   if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
+			die("Could not connect");
+	   }
+
+		$query = "SELECT wine_name, variety, year, winery_name, region_name, cost, sum(qty), on_hand, sum(price)
+          From wine, grape_variety, winery, region, inventory, items, wine_variety , orders
 		  Where wine.winery_id = winery.winery_id
 		    And winery.region_id = region.region_id
 			And inventory.wine_id = wine.wine_id
 			And wine.wine_id = items.wine_id
 			And wine.wine_id = wine_variety.wine_id
-			And grape_variety.variety_id = wine_variety.variety_id";
+			And grape_variety.variety_id = wine_variety.variety_id
+			And items.order_id = orders.order_id
+			And items.cust_id = orders.cust_id";
 
 
-		$wine_name = $_GET['wine_name'];
-		if(!empty($wine_name)){
-			$query .= " AND wine_name LIKE '%wine_name%'";
+		$WineName = $_GET['WineName'];
+		$GrapeVariety = $_GET['GrapeVariety'];
+		$StartYear = intval($_GET['StartYear']);
+		$EndYear = intval($_GET['EndYear']);
+		$WineryName = $_GET['WineryName'];
+		$RegionName = $_GET['RegionName'];
+		$MinStock = intval($_GET['MinStock']);
+		$MinOrder = intval($_GET['MinOrder']);
+		$MinCost = intval($_GET['MinCost']);
+		$MaxCost = intval($_GET['MaxCost']);
+
+		if($MinCost > $MaxCost){
+			die("Mincost is bigger than Maxcost");
+		}
+		if($StartYear > $EndYear){
+			die("Startyear is later than Endyear");
 		}
 
-		$grape_variety = $_GET['grape_variety'];
-		if(!empty($grape_variety)){
-			$query .= " AND variety = '$grape_variety'";
+		if (isset($RegionName) && ($RegionName != "All")) {
+			$query .= " AND region_name like '{$RegionName}%'";
+		}
+		if (isset($WineName) && !empty($WineName)) {
+			$query .= " AND wine_name like '{$WineName}%'";
+		}
+  
+		if (isset($WineryName) && !empty($WineryName)) {
+			$query .= " AND winery_name like '{$WineryName}%'";
+		}
+  
+		if (isset($GrapeVariety) && !empty($GrapeVariety)) {
+			$query .= " AND variety = '{$GrapeVariety}'";
+		}
+  
+		if (isset($StartYear) && isset($EndYear) &&$StartYear < $EndYear) {
+			$query .= " AND year between '{$StartYear}' and '{$EndYear}'";
 		}
 
-		$start_year = $_GET['start_year'];
-		$end_year = $_GET['end_year'];
-		if(!empty($start_year) && !empty($end_year)){
-			$query .= " AND year between $start_year and $end_year";
+		if (isset($MinStock) && !empty($MinStock)) {
+			$query .= " AND on_hand > '{$MinStock}'";
 		}
 
-		$winery_name = $_GET['winery_name'];
-		if(!empty($winery_name)){
-			$query .= " AND winery_name LIKE '%winery_name%'";
+		if (isset($MinOrder) && !empty($MinOrder)) {
+			$query .= " AND qty > '{$MinOrder}'";
 		}
 
-		$region_name = $_GET['region_name'];
-		if(!empty($region_name)){
-			$query .= " AND region = '$region_name'";
+		if (isset($MinCost) && isset($MinCost) && !empty($MinCost) && MinCost < MaxCost){
+			$query .= " AND cost between '{$MinCost}' and '{$MaxCost}'";
 		}
 
-		$min_stock = $_GET['min_stock'];
-		if(!empty($min_stock)){
-			$query .= " AND inventory.on_hand >= $min_stock";
-		}
+		$query .= " GROUP BY wine.wine_id";
+		$query .= " ORDER BY wine.wine_name";
 
-		$min_order = $_GET['min_order'];
-		if(!empty($min_order)){
-			$query .= " AND item.qty >= $min_order";
-		}
-
-		$min_cost = $_GET['min_cost'];
-		$max_cost = $_GET['max_cost'];
-		if(!empty($min_cost) && !empty($max_cost)){
-			$query .= " AND (inventory.cost between '$min_cost' and '$max_cost')";
-		}
-
-		$query .= " GROUP BY wine_name";
-
-
-	 
 	 display_table($connection, $query);
 
-      //Close the database connection
-      mysql_close($connection);
 
 ?>
 
